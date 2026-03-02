b33bbe39a6990de60a4c409ac2a62238881504f7
MARCO CEBRIAN MARIRRODRIGA <marcocebrian55@gmail.com>
2026-03-02 11:22:52 +0000

    modelos-rutas-api

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,11 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User,Exercise,Muscle,Equipment
+from api.models import db, User, Exercise, Muscle, Equipment
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import requests
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -21,48 +23,82 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+
 @api.route('/singup', methods=['POST'])
-def singup():
+def signup():
     data = request.get_json()
-    email = data.get ('email')
-    password = data.get ('password')
+    email = data.get('email')
+    password = data.get('password')
 
     if not email or not password:
-        return jsonify({"error":"Email and password required"}), 400
-    
+        return jsonify({"error": "Email and password required"}), 400
+
     existing_user = db.session.execute(db.select(User).where(
         User.email == email)).scalar_one_or_none()
-    
+
     if existing_user:
-        return jsonify({"error":"Email already exist"}), 400
-    
+        return jsonify({"error": "Email already exist"}), 400
+
     new_user = User(email=email)
     new_user.set_password(password)
 
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({"Message":"User created usccessfully"}), 201
+    return jsonify({"Message": "User created usccessfully"}), 201
+
 
 @api.route('/login', methods=['POST'])
 def login():
 
     data = request.get_json()
-    email = data.get ('email')
-    password = data.get ('password')
+    email = data.get('email')
+    password = data.get('password')
 
     if not email or not password:
-        return jsonify({"error":"Email and password are required"}), 400
-    
+        return jsonify({"error": "Email and password are required"}), 400
+
     user = db.session.execute(db.select(User).where(
         User.email == email)).scalar_one_or_none()
-    
+
     if user is None:
-        return jsonify ({"error": "Invalided email or password"}), 401
-    
+        return jsonify({"error": "Invalided email or password"}), 401
+
     if user.check_password(password):
-        access_token = create_access_token(identity = str(user.id))
-        return jsonify({"Message":"Login successful", "token": access_token, "user": user.serialize()}), 200
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify({"Message": "Login successful", "token": access_token, "user": user.serialize()}), 200
 
     else:
-        return jsonify ({"error": "Invalided email or password"}), 401
+        return jsonify({"error": "Invalided email or password"}), 401
+
+
+@api.route('/import-wger', methods=['GET'])
+def import_wger():
+
+    url = "https://wger.de/api/v2/exerciseinfo/?language=2&status=2"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return jsonify({"msg": "No pude conectar con la Api"}), 500
+
+    data = response.json()
+    print(data)
+    ejercicios_lista = data.get('results', [])
+    print(f"He recibido {len(ejercicios_lista)} ejercicios de la API")
+    for item in ejercicios_lista:
+        translations = item.get('translations')
+
+        if translations is None or translations == "":
+            continue
+
+        for translation in translations:
+            if translation["language"] == 4:
+                nuevo_ejercicio = Exercise(
+                    name=translation["name"],
+                    description=translation["description"]
+
+                )
+                db.session.add(nuevo_ejercicio)
+
+    db.session.commit()
+    return jsonify({"msg": "Se han guardado los ejercicios nuevos"}), 200
