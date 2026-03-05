45c23c2fddc83edc979541a9c120a683f8d6ae7b
MARCO CEBRIAN MARIRRODRIGA <marcocebrian55@gmail.com>
2026-03-05 10:04:03 +0000

    avance_filtros_gen

--- src/api/services.py
+++ src/api/services.py
@@ -2,4 +2,15 @@
 from api.models import db,Equipment,Exercise,Muscle,Workout,WorkoutExercise
 
 
-def generate_workout (user_id,muscle_id,equipment_id,max_time):
+def generate_workout (user_id,muscle_id,equipment_ids,max_time):
+    
+    query = Exercise.query.filter(Exercise.muscles.any(id=muscle_id))
+    
+    if equipment_ids:
+        query = query.filter(Exercise.equipments.any(Equipment.id.in_(equipment_ids)))
+    
+    posibles_exercises = query.all()
+
+    if not posibles_exercises:
+            return None
+        
