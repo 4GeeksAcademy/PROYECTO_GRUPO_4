a637b440a2446aa4614ebc892dfaaab405b36f1f
ManuelFMG <manuel_psp86@hotmail.com>
2026-03-14 22:03:31 +0000

    Navbar con Modales

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,28 +1,56 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/img/Logo-png.png";
 
+import { LoginModal } from "./LoginModal";
+import { RegisterModal } from "./RegisterModal";
+
 export const Navbar = () => {
 
-	return (
-		<nav className="navbar navbar-expand-lg navbar-dark custom-navbar fixed-top">
-      <div className="container">
+    const [showLogin, setShowLogin] = useState(false);
+    const [showRegister, setShowRegister] = useState(false);
 
-        <a className="navbar-brand" href="home.html">
-          <img src={logo} alt="Logo" className="logo-img" />
-        </a>
+    return (
 
-        <div className="ms-auto">
-          <a href="login.html" className="btn custom-btn me-2">
-            Iniciar Sesión
-          </a>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark custom-navbar fixed-top">
+                <div className="container">
 
-          <a href="register.html" className="btn custom-btn">
-            Registrarse
-          </a>
-        </div>
+                    <Link className="navbar-brand" to="/">
+                        <img src={logo} alt="Logo" className="logo-img" />
+                    </Link>
 
-      </div>
-    </nav>
+                    <div className="ms-auto">
 
-	);
+                        <button
+                            className="btn custom-btn me-2"
+                            onClick={() => setShowLogin(true)}
+                        >
+                            Iniciar Sesión
+                        </button>
+
+                        <button
+                            className="btn custom-btn"
+                            onClick={() => setShowRegister(true)}
+                        >
+                            Registrarse
+                        </button>
+
+                    </div>
+
+                </div>
+            </nav>
+
+            <LoginModal
+                show={showLogin}
+                onClose={() => setShowLogin(false)}
+            />
+
+            <RegisterModal
+                show={showRegister}
+                onClose={() => setShowRegister(false)}
+            />
+
+        </>
+    );
 };
